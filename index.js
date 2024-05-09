0909ea931731cb8779cb5e862b8d416e54e04d45
rph <rhansen0127@gmail.com>
2024-05-09 13:02:03 -0500

    added the questions for inquirer

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-import inquirer from "inquirer"; 
-import fs from "fs"
+import inquirer from "inquirer";
+import { Circle, Square, Triangle } from "./lib/shape.js";
 
 const questions = [
   {
-    type: 'input',
-    name: 'letters',
-    message: 'What are the three letters you would like in your logo'
+    type: "input",
+    name: "logoText",
+    prompt: "Please enter up to 3 characters for the text in the logo",
   },
   {
-    type: 'input',
-    name: 'color',
-    message: 'What color would you like your logo to be?'
+    type: "input",
+    name: "textColor",
+    prompt:
+      "Please enter the color that you would like your text to be in the logo",
   },
   {
-    type: 'list',
-    name: 'shape',
-    message: 'What shape would you like your logo to be?',
-    choices: ['Square', 'Circle', 'Triangle'] 
-  }
-]
-
-function init() {
-  inquirer
-  .prompt(questions)
-  .then((answers) => {
-      console.log(answers)
-    })
-}
-
-init();
+    type: "list",
+    name: "logoShape",
+    prompt: "Please select the shape of the logo",
+    choices: ["Circle", "Square", "Triangle"],
+  },
+  {
+    type: "input",
+    name: "logoColor",
+    prompt: "Please enter the color you would like the logo to be",
+  },
+];
+inquirer.prompt(questions).then((answers) => {
+  console.log(answers);
+});
